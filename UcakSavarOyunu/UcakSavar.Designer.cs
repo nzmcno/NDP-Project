2a1f004c5a93c82d303e9f13cafa0756a82c10a9
Nazim <mykaram_fenerli@hotmail.com>
2017-04-24 20:18:52 +0300

    Plane can move right and left.

--- UcakSavarOyunu/UcakSavar.Designer.cs
+++ UcakSavarOyunu/UcakSavar.Designer.cs
@@ -28,7 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UcakSavar));
+            this.defansUcagi = new System.Windows.Forms.PictureBox();
+            this.timerMoveDef = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.defansUcagi)).BeginInit();
             this.SuspendLayout();
+            // 
+            // defansUcagi
+            // 
+            this.defansUcagi.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.defansUcagi.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("defansUcagi.BackgroundImage")));
+            this.defansUcagi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.defansUcagi.Location = new System.Drawing.Point(245, 424);
+            this.defansUcagi.Name = "defansUcagi";
+            this.defansUcagi.Size = new System.Drawing.Size(74, 54);
+            this.defansUcagi.TabIndex = 0;
+            this.defansUcagi.TabStop = false;
+            // 
+            // timerMoveDef
+            // 
+            this.timerMoveDef.Enabled = true;
+            this.timerMoveDef.Tick += new System.EventHandler(this.timerMoveDef_Tick);
             // 
             // UcakSavar
             // 
@@ -36,13 +58,20 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(576, 478);
+            this.Controls.Add(this.defansUcagi);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "UcakSavar";
             this.Text = "Ucak Savar";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.UcakSavar_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.defansUcagi)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox defansUcagi;
+        private System.Windows.Forms.Timer timerMoveDef;
     }
 }
 
--- UcakSavarOyunu/UcakSavar.cs
+++ UcakSavarOyunu/UcakSavar.cs
@@ -10,11 +10,46 @@ using System.Windows.Forms;
 
 namespace UcakSavarOyunu
 {
+   
     public partial class UcakSavar : Form
-    {
+    { int movePlane = 10;
+       
+
         public UcakSavar()
         {
             InitializeComponent();
+            
+        }
+
+        private void timerMoveDef_Tick(object sender, EventArgs e)
+        {
+
+            
+            
+            Invalidate();
+
+        }
+
+        private void UcakSavar_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+
+            if (e.KeyCode == Keys.Right)
+            {
+                if (defansUcagi.Location.X < this.Width - 90)
+                {
+                    defansUcagi.SetBounds(defansUcagi.Location.X + movePlane, defansUcagi.Location.Y, defansUcagi.Width, defansUcagi.Height);
+                }
+            }
+            
+            if (e.KeyCode == Keys.Left)
+            {
+                    if (defansUcagi.Location.X >0)
+                    {
+                        defansUcagi.SetBounds(defansUcagi.Location.X - movePlane, defansUcagi.Location.Y, defansUcagi.Width, defansUcagi.Height);
+                    }
+            }
+            
         }
     }
 }
